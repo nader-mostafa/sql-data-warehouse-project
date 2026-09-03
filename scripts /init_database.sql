d162abaf639dff7c4d460925f6fb716d8421e987
nader mostafa <nader.mostafa.esmail@gmail.com>
2026-09-03 05:45:56 +0300

    Create init_database.sql

---
+++ scripts /init_database.sql
@@ -0,0 +1,16 @@
+use master ;
+go
+-- create database 
+create database datawarehouse ;
+use datawarehouse ;
+
+--create schema 
+
+create schema bronze ;
+go 
+
+create schema silver ;
+go 
+
+create schema gold ;
+go
